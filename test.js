1b8d58445db72b4924f5ebcdb1a92567554412d8
Martin Johnstone <martinejohnstone@gmail.com>
2017-04-18 14:10:25 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,27 @@
+var modeler = require('./modeler');
+var mysql = require('mysql2');
+var fs = require('fs');
+var path = require('path');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'test',
+  password : 'test',
+  database : 'test',
+	rowsAsArray: false,
+	multipleStatements: true
+});
+
+var modelsPath = path.resolve(__dirname) + "/exampleModels/";
+
+function saveModels(model){
+	fs.writeFile(modelsPath + model.def.name + ".js", JSON.stringify(model, null, 2), function(err) {
+		//do something with result here?
+		console.log(modelsPath + model.def.name + ".js", err);
+	});
+}
+
+modeler.init(connection, 'sipclub', saveModels, function(err){
+	if (err) console.log("err: ", err);
+	else console.log("done - check your file output directory for schemas");
+})
